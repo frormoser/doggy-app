6d1c566cf8bdf1db34a64fa64a97ad710276c69a
Federico <joelrormoser@gmail.com>
2022-05-26 19:56:48 -0300

    App developed and subject to future changes

---
+++ src/components/Card.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Spinner from "./Spinner";
+
+const Card = ({ dog, loading, updateDog }) => {
+
+   if(loading) return <Spinner/>
+
+  return (
+    <div className="card bounce" onClick={() => updateDog(dog.breed.id)}>
+      <img src={dog.image} alt="dog" />
+      <p>{dog.breed.name}</p>
+      <p>{dog.breed.life_span}</p>
+    </div>
+  );
+};
+
+export default Card;
